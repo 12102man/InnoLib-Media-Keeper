fbcf2f01b301e03abc90472514d54e252cbac884
12102man <12102man@gmail.com>
2018-03-05 14:43:11 +0300

    Added Running Details file

--- Config/config.py
+++ Config/config.py
@@ -1,5 +1,5 @@
-#token = '342086039:AAE-rBthwuwaUlHH0Xbea6ZnU5Zw7zJ05X4'
-# Debug
+#token = '420182059:342086039:AAE-rBthwuwaUlHH0Xbea6ZnU5Zw7zJ05X4'
+# ILMK
 token = '420182059:AAGF7cwnrfm8FObtZS4RPmU4gVvWuVQZ2Ug'
 
 db_host = '37.46.132.57'
