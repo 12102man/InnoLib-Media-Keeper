af5c287af9c4f5af458475114b0bbb2dc6cf3a65
Mikhail Tkachenko <12102man@gmail.com>
2018-04-25 15:30:50 +0300

    FINAL LAST COMMIT

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 #token = '420182059:342086039:AAE-rBthwuwaUlHH0Xbea6ZnU5Zw7zJ05X4'
 # ILMK
-token = '420182059:AAEOVT8mPFT6t-vRrlbLGfPgh4hWOjh8ry8'
+token = '342086039:AAG6OYuUOjGomJqiK7YOxMajgGfh56BNmAE'
 
 db_host = '37.46.132.57'
 db_username = 'telebot'
